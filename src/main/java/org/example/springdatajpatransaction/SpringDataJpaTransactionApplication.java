83f9aced36e72dab1c60e9b9f726e78041952d71
rmsnandha <rmsnandha@example.com>
2025-01-07 18:58:32 -0600

    Initial commit

---
+++ src/main/java/org/example/springdatajpatransaction/SpringDataJpaTransactionApplication.java
@@ -0,0 +1,13 @@
+package org.example.springdatajpatransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springdatajpatransaction/SpringDataJpaTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springdatajpatransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
